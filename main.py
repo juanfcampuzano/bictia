de181c2568e38383ef2e8807b81d7efd2d518776
Juan Campuzano <juanfecazuluaga@gmail.com>
2023-05-18 21:51:04 -0500

    Cambiamos de chatgpt a bard

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import json
 from fastapi.middleware.cors import CORSMiddleware
 import re
 from youtubesearchpython import VideosSearch
+from bardapi import Bard
 
 app = FastAPI()
 
@@ -20,50 +21,56 @@ app.add_middleware(
 def post_ruta_educativa(role: str):
     
     role = role.replace('_', ' ')
-    headers = {"Content-Type": "application/json", 'Authorization': 'Bearer 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'}
 
-    url = "https://api.openai.com/v1/chat/completions"
-
-    prompt = f"""Imagina que eres un experto en curriculum designer. Dame un curriculum para aprender a ser """+role+""" con los temarios, ordenados por secciones y por subsecciones, agregando el tiempo aproximado de estudio para cada uno. Quiero la respuesta en formato json {'seccion1':{'subseccion1':{'tema1':'tiempo1}}} no acepto mas formatos, si no me mandas el formato json mi app crashea"""
-
-    data = {
-
-        "model": "gpt-3.5-turbo",
-        "max_tokens": 3500,
-        "messages":[
-                {"role": "system", "content": "You are a chatbot"},
-                {"role": "user", "content": prompt},
-            ]
+    token = 'WwjjqNqGPs1ijNEKQHSEyLjwQDxgCEzLXhd113fhV5jWpHU0V9L0NTpQo8WP2dSbiHWamQ.'
+    query = '''imagine you are a curriculum designer. Please design for me a curriculum for being a '''+role+'''. Provide it in a json format like this example {
+    "Core Curriculum": {
+        "Introduction to Data Analysis": ["What is data analysis?", "The data analysis process", "Data types and structures"],
+        "Data Wrangling": ["Data cleaning", "Data transformation", "Data integration"],
+        "Statistical Analysis": ["Descriptive statistics", "Inferential statistics", "Regression analysis"],
+        "Machine Learning": ["Supervised learning", "Unsupervised learning", "Deep learning"],
+        "Data Visualization": ["Creating data visualizations", "Interpreting data visualizations"]
+    },
+    "Technical Skills": {
+        "Programming": ["Python", "SQL"],
+        "Data Science Tools": ["R", "Tableau", "Power BI"],
+        "Cloud Computing": ["AWS", "Azure", "Google Cloud Platform"]
+    },
+    "Soft Skills": {
+        "Communication": ["Presenting data", "Writing reports"],
+        "Problem Solving": ["Identifying problems", "Developing solutions"],
+        "Critical Thinking": ["Analyzing data", "Making decisions"],
+        "Teamwork": ["Working with others", "Collaborating on projects"]
     }
+    }'''
 
-    response = requests.post(url, headers=headers, json=data)
+    bard = Bard(token=token)
+    bard.get_answer(query)['content']
 
-    response_dict = json.loads(response.text)
+    string = bard.get_answer(query)['content']
+    first_curly = string.find('{')
+    last_curly = string.rfind('}')
+    string = string[first_curly:last_curly+1]
 
-    print(response_dict)
-
-
-    respuesta_chatgpt = response_dict['choices'][0]['message']['content']
-    respuesta_chatgpt = re.sub(r'\n\s*', '', respuesta_chatgpt)
-
-    regex = r'^[^{]*({[\s\S]*})[^}]*$'
-
-    print(respuesta_chatgpt)
-
-    try:
-        json_str = re.findall(regex, respuesta_chatgpt)[0]
-    except:
-        respuesta_chatgpt = respuesta_chatgpt+'}'
-        json_str = re.findall(regex, respuesta_chatgpt)[0]
-
-    loaded_json = json.loads(json_str)
+    loaded_json = json.loads(re.sub(' +', ' ', string.replace('\n','')).replace(', }','}').replace(',}','}'))
 
     ruta_educativa = []
 
     for seccion in loaded_json.keys():
         seccion_actual = loaded_json[seccion]
-        for subseccion in seccion_actual.keys():
-            subseccion_actual = seccion_actual[subseccion]
+
+        if type(seccion_actual) == dict:
+            subsecciones = seccion_actual.keys()
+        elif type(seccion_actual) == list:
+            subsecciones = seccion_actual
+        else:
+            subsecciones = seccion_actual.keys()
+
+        for subseccion in subsecciones:
+            if type(seccion_actual) == dict:
+                subseccion_actual = seccion_actual[subseccion]
+            else:
+                subseccion_actual = subseccion
             if type(subseccion_actual) == str:
                 row = {}
                 videosSearch = VideosSearch(subseccion, limit = 1)
@@ -73,7 +80,7 @@ def post_ruta_educativa(role: str):
                 row['titulo_video'] = [result['title']]
                 row['url_video'] = [result['link']]
             else:
-                for tema in subseccion_actual.keys():
+                for tema in subseccion_actual:
                     row = {}
                     videosSearch = VideosSearch(tema, limit = 1)
                     result = videosSearch.result()['result'][0]
