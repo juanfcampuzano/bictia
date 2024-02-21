7b97698d4f377005d99c5cdc173c3ed8877139f3
Juan Campuzano <juanfecazuluaga@gmail.com>
2024-02-21 17:45:51 -0500

    Curso de coursera

--- main.py
+++ main.py
@@ -204,14 +204,6 @@ async def save_chatgpt_query(request: ChatGPTRequest, background_tasks: Backgrou
             tries += 1
             continue
 
-    # chatgpt_responses = {}
-    # temp_dict = {}
-    # temp_dict['role']=role
-    # temp_dict['response']=answer
-    # chatgpt_responses[id_user] = temp_dict
-    # save_to_local(chatgpt_responses, 'chatgpt_responses')
-    # save_to_s3(chatgpt_responses, 'chatgpt_responses')
-
     background_tasks.add_task(nueva_ruta_educativa_bbits, role, id_user)
 
     return {"message": "respuesta agregada correctamente"}
@@ -310,13 +302,6 @@ def nueva_ruta_educativa(role: str):
                     tries += 1
                     continue
 
-            # rutas_educativas = {}
-            # temp_dict = {}
-            # temp_dict['ruta']=ruta_educativa
-            # rutas_educativas[id_user] = temp_dict
-            # save_to_local(rutas_educativas, 'rutas_educativas')
-            # save_to_s3(rutas_educativas, 'rutas_educativas')
-            # break
             break
         except Exception as e:
             print('ERROR GENERANDO RUTA EDUCATIVA')
@@ -328,9 +313,9 @@ def nueva_ruta_educativa(role: str):
 
 def nueva_ruta_educativa_bbits(role: str, id_user: str):
 
-    # ruta = ruta_educativa_bbits(role)
+    ruta = ruta_educativa_bbits(role)
 
-    ruta = nueva_ruta_educativa(role)
+    # ruta = nueva_ruta_educativa(role)
 
     tries = 0
 
@@ -352,37 +337,6 @@ def nueva_ruta_educativa_bbits(role: str, id_user: str):
 
 @app.get("/{role}")
 def post_ruta_educativa(role: str):
-    
-    # role = role.replace('_', ' ')
-
-    # token = 'XAjmxb_yUG3YuN-LYn-vqzvHh7wMrRdyjgalK3-KYHHsvodXv_LsYDV0rChI8l3r7N_JyQ.'
-    # query = '''imagine you are a curriculum designer. Please design for me a curriculum for being a '''+role+'''. Provide it in a json format like this example {
-    # "Core Curriculum": {
-    #     "Introduction to Data Analysis": ["What is data analysis?", "The data analysis process", "Data types and structures"],
-    #     "Data Wrangling": ["Data cleaning", "Data transformation", "Data integration"],
-    #     "Statistical Analysis": ["Descriptive statistics", "Inferential statistics", "Regression analysis"],
-    #     "Machine Learning": ["Supervised learning", "Unsupervised learning", "Deep learning"],
-    #     "Data Visualization": ["Creating data visualizations", "Interpreting data visualizations"]
-    # },
-    # "Technical Skills": {
-    #     "Programming": ["Python", "SQL"],
-    #     "Data Science Tools": ["R", "Tableau", "Power BI"],
-    #     "Cloud Computing": ["AWS", "Azure", "Google Cloud Platform"]
-    # },
-    # "Soft Skills": {
-    #     "Communication": ["Presenting data", "Writing reports"],
-    #     "Problem Solving": ["Identifying problems", "Developing solutions"],
-    #     "Critical Thinking": ["Analyzing data", "Making decisions"],
-    #     "Teamwork": ["Working with others", "Collaborating on projects"]
-    # }
-    # }'''
-
-    
-
-    # bard = Bard(token=token)
-    # bard.get_answer(query)['content']
-
-    # string = bard.get_answer(query)['content']
 
     openai.api_key = os.environ["OPENAIKEY"]
     carreer = role
@@ -479,85 +433,6 @@ def get_ruta_educativa(user_id: str):
             continue
     return{"message":"error"}
 
-# @app.get("/ruta_educativa_bbits/{role}")
-# def post_ruta_educativa_bbits(role: str):
-#     def loadJSONFile(file):
-#         docs=[]
-#         # Load JSON file
-#         data = json.load(file)
-
-#         # Iterate through 'pages'
-#         for curso in data:
-#             id = curso['id']
-#             precio = curso['precio']
-#             duracion = curso['duracion']
-#             categoria = curso['categoria']
-#             titulo = curso['titulo']
-#             contenido = curso['contenido']
-#             metadata={"id":id}
-
-#             # Process snippets for each page
-#             contenidos_temas = []
-#             titulos_temas = []
-#             for tema in contenido:
-#                 titulo_tema = tema['titulo']
-#                 contenido_tema = tema['contenido']
-
-#                 contenidos_temas.append(contenido_tema)
-#                 titulos_temas.append(titulo_tema)
-
-
-#             docs.append(Document(page_content= 'id: ' + str(id) + ' titulo: ' + titulo + ' categoria: ' + categoria + ' ' + ' contenidos ' +  ' '.join(titulos_temas) + ' ' + ' '.join(contenido_tema), metadata=metadata))
-#         return docs 
-    
-#     docs = loadJSONFile(open('docs_bbits/Cursos.json', 'rb'))
-#     embeddings = OpenAIEmbeddings()
-#     db = DocArrayInMemorySearch.from_documents(
-#     docs, 
-#     embeddings
-#     )
-#     llm = ChatOpenAI(temperature = 0.0)
-#     retriever = db.as_retriever(search_kwargs={"k": 10})
-
-#     qa_stuff = RetrievalQA.from_chain_type(
-#     llm=llm, 
-#     chain_type="stuff", 
-#     retriever=retriever, 
-#     verbose=True
-#     )
-
-#     query =  f"Listame todos los cursos que me orientarán a ser un {role} si no tienes cursos aropiados no inventes respuestas, solo di que no sabes. Tienes que incluir el titulo y el id del curso\
-#     en markdown y resume cada uno."
-
-#     response = qa_stuff.run(query)
-
-#     review_template = """\
-#     Para el siguiente texto que está en formato markdown y contiene información de varios cursos, extrae la siguiente información para cada curso:
-
-#     id: ¿Cuál es el id del curso? \
-#     Si esta información no se encuentra, el valor debe ser -1.
-
-#     titulo: ¿Cuál es el título del curso? \
-#     Si esta información no se encuentra, el valor debe ser -1.
-
-#     descripcion: ¿Cuál es el resumen del curso? \
-#     Si esta información no se encuentra, el valor debe ser -1.
-
-#     Formatea la salida como lista de JSON con las siguientes claves para cada curso:
-#     id
-#     titulo
-#     descripcion
-
-#     texto: {text}
-#     """
-#     prompt_template = ChatPromptTemplate.from_template(review_template)
-
-#     messages = prompt_template.format_messages(text=response)
-#     formated_response = llm(messages)
-
-#     respuesta = ast.literal_eval(formated_response.content)
-
-#     return respuesta
 
 def ruta_educativa_bbits(role: str):
     def loadJSONFile(file):
@@ -615,6 +490,9 @@ def ruta_educativa_bbits(role: str):
     Para el siguiente texto que está en formato markdown y contiene información de varios cursos, extrae la siguiente información para cada curso:
 
     id: ¿Cuál es el id del curso? \
+    Si esta información no se encuentra, el valor debe ser -1.
+
+    url: ¿Cuál es el link al curso? \
     Si esta información no se encuentra, el valor debe ser -1.
 
     titulo: ¿Cuál es el título del curso? \
